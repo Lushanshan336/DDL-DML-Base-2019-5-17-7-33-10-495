bbbcb37097a9b9367f0ae2658f025834d250287d
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-07-02 23:29:20 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+INSERT INTO table_name VALUES (value1, value2,....)
 -- 修改记录  
-
+UPDATE [LOW_PRIORITY][IGNORE] table_reference SET col_name1={expr1|DEFAULT}[,col_name2={expr2|DEFAULT}]...[WHERE where_condition]
 --  删除记录  
-
+DELETE FROM tbl_name [WHERE where_condition]
 -- 查询记录  
+SELECT @rownum:=@rownum+1 AS rownum, table_name.* FROM (SELECT @rownum:=0) r, table_name
